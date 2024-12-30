fede73b1e88c74778bfb35dd4d08447ba4474ab0
Julieth221 <darlycuberos17@gmail.com>
2024-12-30 15:48:21 -0500

    Configurar el servicio de correo

--- backend/src/mail/mail.module.ts
+++ backend/src/mail/mail.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { mailConfig } from '../config/mail.config';
 import { MailService } from './mail.service';
 
 @Module({
   imports: [
-    MailerModule.forRoot(mailConfig)
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => mailConfig(configService),
+      inject: [ConfigService],
+    })
   ],
   providers: [MailService],
   exports: [MailService]
