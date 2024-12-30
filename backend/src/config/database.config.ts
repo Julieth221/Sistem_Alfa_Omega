7fbd67b0e06e09b0847a777b789a520b9fc4abc6
Julieth221 <darlycuberos17@gmail.com>
2024-12-29 22:14:21 -0500

    Se configuro el archivo principal y el database para la coneccion del frontend con el backend

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -1,18 +1,17 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { config } from 'dotenv';
+import * as dotenv from 'dotenv';
 
-config();
+dotenv.config();
 
 export const databaseConfig: TypeOrmModuleOptions = {
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    schema: process.env.DB_SCHEMA,
-    entities: ['dist/**/*.entity{.ts,.js}'],
-    synchronize: process.env.APP_ENV === 'development',
-    logging: process.env.APP_ENV === 'development',
-    ssl: process.env.APP_ENV === 'production',
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD || 'sistem',
+  database: process.env.DB_NAME,
+  schema: process.env.DB_SCHEMA,
+  synchronize: false,
+  logging: true,
+  entities: [__dirname + '/../**/*.model{.ts,.js}'],
 };
