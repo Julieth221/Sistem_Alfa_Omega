4df40a9860b5402f76230336a2340d11d39bb48b
Julieth221 <darlycuberos17@gmail.com>
2025-01-03 21:55:46 -0500

    Configuración en el backend y módulos

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,19 +1,33 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 import { databaseConfig } from './config/database.config';
+import { NovedadesModule } from './modules/novedades.module';
+import { AuthModule } from './auth/auth.module';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot(databaseConfig),
+    NovedadesModule,
     AuthModule,
+    MailerModule.forRootAsync({
+      useFactory: () => ({
+        transport: {
+          host: process.env.MAIL_HOST,
+          secure: false,
+          auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASSWORD,
+          },
+        },
+        defaults: {
+          from: process.env.MAIL_FROM,
+        },
+      }),
+    })
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
 
