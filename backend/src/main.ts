2693c4c414de96125ec0c114680ecb07dd98ef1c
Julieth221 <darlycuberos17@gmail.com>
2025-01-03 21:59:47 -0500

    Configuracion del archivo principal e interface de la novedad

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
-  app.enableCors({
-    origin: 'http://localhost:4200',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+  // Aumentar el límite de tamaño de la petición
+  app.use(bodyParser.json({limit: '50mb'}));
+  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+  
+  // Configurar CORS
+  app.enableCors();
+  
+  // Configurar carpeta de archivos estáticos
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
   });
-  
-  app.useGlobalPipes(new ValidationPipe());
-  
+
   await app.listen(3000);
   console.log(`Servidor corriendo en: http://localhost:3000`);
 }
