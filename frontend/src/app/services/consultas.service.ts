30c2072069c1cd43af6c8fbaed7d99c32c7819b5
Julieth221 <darlycuberos17@gmail.com>
2025-01-24 21:52:30 -0500

    configuracion del backend para el preview PDF

--- frontend/src/app/services/consultas.service.ts
+++ frontend/src/app/services/consultas.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,23 +11,39 @@ export class ConsultasService {
 
   constructor(private http: HttpClient) {}
 
-  getNovedades() {
-    return this.http.get(`${this.apiUrl}/consulta`);
+  getNovedades(filtro?: string): Observable<any> {
+    let url = `${this.apiUrl}/consulta`;
+    if (filtro) {
+      url += `?remision_factura=${filtro}`;
+    }
+    return this.http.get(url);
   }
 
-  getProductosNovedad(novedadId: number) {
+  getNovedad(id: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
+
+  getProductosNovedad(novedadId: number): Observable<any> {
     return this.http.get(`${this.apiUrl}/${novedadId}/productos`);
   }
 
-  actualizarNovedad(id: number, novedad: any) {
+  actualizarNovedad(id: number, novedad: any): Observable<any> {
     return this.http.put(`${this.apiUrl}/${id}`, novedad);
   }
 
-  eliminarNovedad(id: number) {
+  eliminarNovedad(id: number): Observable<any> {
     return this.http.delete(`${this.apiUrl}/${id}`);
   }
 
-  agregarObservacion(novedadId: number, observacion: string) {
+  agregarObservacion(novedadId: number, observacion: string): Observable<any> {
     return this.http.post(`${this.apiUrl}/${novedadId}/observaciones`, { observacion });
+  }
+
+  getObservaciones(novedadId: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${novedadId}/observaciones`);
+  }
+
+  enviarCorreo(novedadId: number): Observable<any> {
+    return this.http.post(`${this.apiUrl}/${novedadId}/enviar-correo`, {});
   }
 } 
