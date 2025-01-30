8f7cc2383d3ac4e1d9bb97f92267e88093b1ab86
Julieth221 <darlycuberos17@gmail.com>
2025-01-30 16:17:43 -0500

    Configuración de database.config

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -4,15 +4,16 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
+// Configuración para usar con TypeOrmModule
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'sistem',
-  database: process.env.DB_NAME || 'SISTEMA_ALFA_OMEGA',
-  schema: process.env.DB_SCHEMA || 'SistemNovedad',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  schema: process.env.DB_SCHEMA,
   entities: [join(__dirname, '..', '**', '*.entity{.ts,.js}')],
   synchronize: false,
-  logging: true,
+  logging: true, // Siempre en false por seguridad
 };
